a7e53bebe23dffbebaa2d495ab15b8e075f493b2
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-09-12 13:49:11 +0200

    Add project files.

---
+++ uppgift 1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace uppgift_1
+{
+
+  class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Här är lite text");
+
+            // Efter två snedstreck så kan man skriva vad man vill
+            // Detta är en kommentar
+            Console.WriteLine("Denna text kommer på en ny rad");
+
+            /*
+             * karam är hora
+             * musse har stor näsa
+             */
+            Console.WriteLine("Hooyo ku jeclahay");
+            // Console.WriteLine(Hooyoo love you);
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+      
+
+
+
+
+  
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
